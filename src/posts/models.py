8b26b5541d3ea40781dc91a2653d670794e6c443
josephturi <josephturi@gmail.com>
2017-06-21 15:14:26 +0100

    Q - queryset added to searh

--- src/posts/models.py
+++ src/posts/models.py
@@ -2,13 +2,20 @@ from __future__ import unicode_literals
 
 from django.conf import settings
 from django.core.urlresolvers import reverse
+from django.utils import timezone
 from django.db import models
 from django.db.models.signals import pre_save
 
 from django.utils.text import slugify
 
-# Create your models here.
+
 # MVC Model View Controller
+
+class PostManager(models.Manager):
+    def active(self, *args, **kwargs):
+        # queryset_list = Post.objects.all() = super(PostManager, self).all()
+        return super(PostManager, self).filter(draft=False).filter(publish__lte=timezone.now())
+
 def upload_location(instance, filename):
     return "%s/%s" %(instance.id, filename)
     # filebase, extension=filename.split(".")
@@ -30,6 +37,8 @@ class Post(models.Model):
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
     draft = models.BooleanField(default=False)
     publish = models.DateField(auto_now=False, auto_now_add=False)
+
+    objects = PostManager()
 
     def __unicode__(self):
         return self.title
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,12 +1,13 @@
 from urllib.parse import quote_plus
 from django.contrib import messages
 from django.utils import timezone
+from django.db.models import Q
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404, redirect
 from .forms import PostForm
 from .models import Post
-# Create your views here.
+
 
 def post_create(request):
     if not request.user.is_staff or not request.user.is_superuser:
@@ -36,7 +37,15 @@ def post_create(request):
     return render(request, "post_form.html", context)
 
 def post_list(request):
-    queryset_list = Post.objects.all().filter(draft=False).filter(publish__lte=timezone.now())#.order_by("-timestamp")
+    queryset_list = Post.objects.active()#.order_by("-timestamp")
+    if request.user.is_staff or request.user.is_superuser:
+        queryset_list = Post.objects.all()
+    query = request.GET.get("q")
+    if query:
+        queryset_list = queryset_list.filter(
+            Q(title__icontains=query) |
+            Q(content__icontains=query)
+            )
     paginator = Paginator(queryset_list, 7) # Show 7 contacts per page
     page_request_var = "page"
     page = request.GET.get(page_request_var)
@@ -60,6 +69,9 @@ def post_list(request):
 def post_detail(request, slug=None):
     # instance = Post.objects.get(pk=1)
     instance = get_object_or_404(Post, slug=slug)
+    if instance.draft or instance.publish > timezone.now().date():
+        if not request.user.is_staff or not request.user.is_superuser:
+            raise Http404
     share_string = quote_plus(instance.content)
     context = {
         "title": instance.title,
